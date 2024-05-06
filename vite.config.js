46543985675cf840fc2b55cd8891a91fbf280e87
ThinLiquid <thnlqd@gmail.com>
2024-05-06 23:05:11 +0100

    whoops

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from "vite";
 import { nodePolyfills } from 'vite-plugin-node-polyfills'
 
 export default defineConfig({
-  base: '/Spotify-Visualizer',
+  base: '/buffer',
   build: {
     target: 'ES2022',
     sourcemap: true
