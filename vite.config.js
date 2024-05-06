8fa37d76ffa8131ba16a91cceb4549083ccd2c50
ThinLiquid <thnlqd@gmail.com>
2024-05-06 23:01:35 +0100

    add build

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,7 @@ import { defineConfig } from "vite";
 import { nodePolyfills } from 'vite-plugin-node-polyfills'
 
 export default defineConfig({
+  base: '/Spotify-Visualizer',
   plugins: [
     nodePolyfills(),
   ],
