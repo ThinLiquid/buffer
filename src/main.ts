878790b3f69a43f0ba37a346d1e66d08979fdd91
ThinLiquid <thnlqd@gmail.com>
2024-05-11 22:02:52 +0100

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -50,7 +50,6 @@ if (params.has('crt')) {
 if (params.has('debug')) {
   ;(function () {
     var src = '//cdn.jsdelivr.net/npm/eruda';
-    if (!/eruda=true/.test(window.location) && localStorage.getItem('active-eruda') != 'true') return;
     document.write('<scr' + 'ipt src="' + src + '"></scr' + 'ipt>');
     document.write('<scr' + 'ipt>eruda.init();</scr' + 'ipt>');
   })();
